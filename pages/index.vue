9d48f7c3be764c2ac7ee8ccb5423ed6d7f9a44f9
Kelvin Mitau <kelvinkyendwa@gmail.com>
2021-04-29 14:08:55 +0300

    Create home page

---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">COVID19 Cases in Eastern Africa</h1>
+      <h2 class="subtitle">
+        Corona virus disease, if we treat this normally it will treat us
+        abnormally
+      </h2>
+      <div class="links">
+        <h3 class="h3">Country List</h3>
+        <div v-for="country in countries" :key="country">
+          <a :href="country">Cases in {{ country }}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      countries: ['Kenya', 'Tanzania', 'Uganda', 'Ethiopia'],
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 55px;
+  color: #35495e;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  margin: 25px 0;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 1.1rem;
+  color: #526488;
+  word-spacing: 2px;
+  padding-bottom: 15px;
+  max-width: 600px;
+}
+
+.subtitle a {
+  font-weight: 500;
+  color: inherit;
+}
+
+.links {
+  padding-top: 15px;
+  margin-bottom: 20px;
+}
+
+.content-logos {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 500px;
+}
+
+.plus {
+  font-size: 2.5rem;
+  margin: 15px;
+  color: #35495e;
+}
+
+.h3 {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 400;
+  margin: 10px;
+}
+</style>
